218d088ebd5af4e956d4a864bfa3808551a07dc8
maiglesias <miglesias@netlabs.com.uy>
2023-11-21 14:07:30 -0300

    added quality gate and fixed it

--- .github/workflows/hhh.sh
+++ .github/workflows/hhh.sh
@@ -1,14 +1,5 @@
-SONAR_TOKEN="a121f9c444947687c9b2afbc49c9974a1220ffe2"
-TASK_ID=$(curl -s -u "${SONAR_TOKEN}:" "https://sonarcloud.io/api/ce/activity" | jq -r '.tasks[0].analysisId')
+QG_STATUS=$(curl https://sonarcloud.io/api/qualitygates/project_status\?projectKey\=maiglesias_NodeGoat | jq -r '.projectStatus.status')
 
-if [ -z "${TASK_ID}" ]; then
-    echo "Error al obtener el ID de la tarea. Deteniendo el pipeline."
-    exit 1
-fi
-
-QG_STATUS=$(curl -s -u "${SONAR_TOKEN}:" "https://sonarcloud.io/api/qualitygates/project_status?analysisId=${TASK_ID}" | jq -r '.projectStatus.status')
-echo ${QG_STATUS}
-if [ "${QG_STATUS}" = "ERROR" ]; then
+if [ ${QG_STATUS} == "ERROR" ]; then
     echo "Quality Gate failed. Stopping the pipeline."
-    exit 1
 fi
