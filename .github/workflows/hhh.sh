b5b182dc24d03f376c71197a81c06746d1f598bf
maiglesias <miglesias@netlabs.com.uy>
2023-11-22 12:00:23 -0300

    added zap test

--- .github/workflows/hhh.sh
+++
@@ -1,5 +0,0 @@
-QG_STATUS=$(curl https://sonarcloud.io/api/qualitygates/project_status\?projectKey\=maiglesias_NodeGoat | jq -r '.projectStatus.status')
-
-if [ ${QG_STATUS} == "ERROR" ]; then
-    echo "Quality Gate failed. Stopping the pipeline."
-fi
